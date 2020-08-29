26480a0dbcb40efd541114d8370e04693336d905
shihab <echoshihab@gmail.com>
2020-08-29 10:05:56 -0400

    Add project files.

---
+++ ParkApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<NationalPark> NationalParks {get;set;}
+
+
+    }
+}
