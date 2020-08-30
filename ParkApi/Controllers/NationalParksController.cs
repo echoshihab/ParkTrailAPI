dd36564cfa58548783c5a43c796325834d294a82
shihab <echoshihab@gmail.com>
2020-08-30 16:53:17 -0400

    implment get all parks in api controller

---
+++ ParkApi/Controllers/NationalParksController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkApi.Models.Dtos;
+using ParkApi.Models.Repository.IRepository;
+
+namespace ParkApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : Controller
+    {
+        private INationalParkRepository _nationalParkRepository;
+        private readonly IMapper _mapper;
+        public NationalParksController(IMapper mapper, INationalParkRepository  nationalParkRepository)
+        {
+            _mapper = mapper;
+            _nationalParkRepository = nationalParkRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetNationalParks()
+        {
+            var nationalParks = _nationalParkRepository.GetNationalParks();
+            var nationalParksDto = new List<NationalParkDto>();
+
+            foreach(var park in nationalParks)
+            {
+                nationalParksDto.Add(_mapper.Map<NationalParkDto>(park));
+            }
+
+            return Ok(nationalParks);
+        }
+    }
+}
