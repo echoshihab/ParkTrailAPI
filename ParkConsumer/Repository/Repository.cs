6ce18e3b051cae8368e436a277634dd34db8de6f
shihab <echoshihab@gmail.com>
2020-09-09 21:53:25 -0400

    add repository interface and implement in consumer

---
+++ ParkConsumer/Repository/Repository.cs
@@ -0,0 +1,36 @@
+﻿using ParkConsumer.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkConsumer.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public Task<bool> CreateAsync(string url, T objToCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(string url, int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(string url, int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(string url, T objToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
