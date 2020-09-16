b2e07cb6b6306927b420f3f6231583be507841a3
shihab <echoshihab@gmail.com>
2020-09-15 21:09:22 -0400

    implement createasync in repository

--- ParkConsumer/Repository/Repository.cs
+++ ParkConsumer/Repository/Repository.cs
@@ -1,16 +1,45 @@
-﻿using ParkConsumer.Repository.IRepository;
+﻿using Newtonsoft.Json;
+using ParkConsumer.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ParkConsumer.Repository
 {
     public class Repository<T> : IRepository<T> where T : class
     {
-        public Task<bool> CreateAsync(string url, T objToCreate)
+        private readonly IHttpClientFactory _clientFactory;
+        public Repository(IHttpClientFactory clientFactory)
         {
-            throw new NotImplementedException();
+            _clientFactory = clientFactory;
+        }
+        public async Task<bool> CreateAsync(string url, T objToCreate)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if (objToCreate != null)
+            {
+                
+                request.Content = new StringContent(JsonConvert.SerializeObject(objToCreate), Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return false;
+            }
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if(response.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+
         }
 
         public Task<bool> DeleteAsync(string url, int Id)
