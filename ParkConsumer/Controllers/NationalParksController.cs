f43a5a20b304eaec4b9cee5c2c46420eae4b9787
shihab <echoshihab@gmail.com>
2020-09-22 20:19:50 -0400

    complete upsert post action in consumer for create and edit

--- ParkConsumer/Controllers/NationalParksController.cs
+++ ParkConsumer/Controllers/NationalParksController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -36,6 +37,48 @@ namespace ParkConsumer.Controllers
 
             return View(obj);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if(ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using var ms1 = new MemoryStream();
+                        fs1.CopyTo(ms1);
+                        p1 = ms1.ToArray();
+                    }
+
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objFromDb = await _nationalParkRepository.GetAsync(SD.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objFromDb.Picture;
+                }
+                if(obj.Id == 0)
+                {
+                    await _nationalParkRepository.CreateAsync(SD.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _nationalParkRepository.UpdateAsync(SD.NationalParkAPIPath+obj.Id, obj);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
 
         public async Task<IActionResult> GetAllNationalPark()
         {
