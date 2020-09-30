809703ae7783511d2a0117b363e92f34645a98de
shihab <echoshihab@gmail.com>
2020-09-29 20:00:13 -0400

    complete index+upsert in consumer trails controllers in

---
+++ ParkConsumer/Controllers/TrailsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ParkConsumer.Models;
+using ParkConsumer.Models.ViewModel;
+using ParkConsumer.Repository.IRepository;
+
+namespace ParkConsumer.Controllers
+{
+    public class TrailsController : Controller
+    {
+        private readonly INationalParkRepository _nationalParkRepository;
+        private readonly ITrailRepository _trailRepository;
+
+        public TrailsController(INationalParkRepository nationalParkRepository,
+            ITrailRepository trailRepository)
+        {
+            _nationalParkRepository = nationalParkRepository;
+            _trailRepository = trailRepository;
+        }
+        public IActionResult Index()
+        {
+            return View(new Trail() { });
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            IEnumerable<NationalPark> npList = await _nationalParkRepository.GetAllAsync(SD.NationalParkAPIPath);
+
+            TrailsVM objVM = new TrailsVM()
+            {
+                NationalParkList = npList.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+
+            };
+
+           
+            if(id == null)
+            {
+                return View(objVM);
+            }
+
+            objVM.Trail = await _trailRepository.GetAsync(SD.TrailAPIPath, id.GetValueOrDefault());
+            if(objVM.Trail == null)
+            {
+                return NotFound();
+            }
+
+            return View(objVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if(ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using var ms1 = new MemoryStream();
+                        fs1.CopyTo(ms1);
+                        p1 = ms1.ToArray();
+                    }
+
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objFromDb = await _nationalParkRepository.GetAsync(SD.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objFromDb.Picture;
+                }
+                if(obj.Id == 0)
+                {
+                    await _nationalParkRepository.CreateAsync(SD.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _nationalParkRepository.UpdateAsync(SD.NationalParkAPIPath+obj.Id, obj);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
+
+        public async Task<IActionResult> GetAllNationalPark()
+        {
+            var data = await _nationalParkRepository.GetAllAsync(SD.NationalParkAPIPath);
+            return Json(new { data });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            var status = await _nationalParkRepository.DeleteAsync(SD.NationalParkAPIPath, id);
+            if (status)
+            {
+                return Json(new { success = true, message = "Delete Successful" });
+
+            }
+            return Json(new { success = false, message = "Delete Not Successful" });
+            
+        }
+    }
+}
