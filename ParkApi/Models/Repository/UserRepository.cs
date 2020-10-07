1cf4ac184e6fbed46acfbc8ddc4612016fa27d1d
shihab <echoshihab@gmail.com>
2020-10-06 20:22:07 -0400

    add logic for creating token in user repository

--- ParkApi/Models/Repository/UserRepository.cs
+++ ParkApi/Models/Repository/UserRepository.cs
@@ -1,8 +1,13 @@
-﻿using ParkApi.Data;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ParkApi.Data;
 using ParkApi.Models.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ParkApi.Models.Repository
@@ -10,14 +15,39 @@ namespace ParkApi.Models.Repository
     public class UserRepository : IUserRepository
     {
         private readonly ApplicationDbContext _db;
+        private readonly AppSettings _appSettings;
 
-        public UserRepository(ApplicationDbContext db)
+        public UserRepository(ApplicationDbContext db, IOptions<AppSettings> appSettings)
         {
             _db = db;
+            _appSettings = appSettings.Value;
         }
         public User Authenticate(string username, string password)
         {
-            throw new NotImplementedException();
+            var user = _db.Users.SingleOrDefault(x => x.UserName == username && x.Password == password);
+
+            //user not found
+            if(user == null)
+            {
+                return null;
+            }
+
+            //if user was found generate JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            return user;
         }
 
         public bool isUniqueUser(string username)
--- ParkApi/Models/User.cs
+++ ParkApi/Models/User.cs
@@ -10,7 +10,7 @@ namespace ParkApi.Models
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-
+        public string Password { get; set; }
         public string Role { get; set; }
         [NotMapped]
         public string Token { get; set; }
