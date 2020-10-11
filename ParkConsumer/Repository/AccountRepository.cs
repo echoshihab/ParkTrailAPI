9c37907d01829f7520852186e92387e263e4ff64
shihab <echoshihab@gmail.com>
2020-10-11 11:49:14 -0400

    add user model and account repository to consumer

---
+++ ParkConsumer/Repository/AccountRepository.cs
@@ -0,0 +1,30 @@
+﻿using ParkConsumer.Models;
+using ParkConsumer.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParkConsumer.Repository
+{
+    public class AccountRepository : Repository<User>, IAccountRepository
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public AccountRepository(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public Task<User> LoginAsync(string url, User objToCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterAsync(string url, User objToCreate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
