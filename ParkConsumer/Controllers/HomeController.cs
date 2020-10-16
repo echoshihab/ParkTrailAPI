0cd5f6d6f9f2e5ed1ca477b5ab0dc6322aa671ba
shihab <echoshihab@gmail.com>
2020-10-16 18:59:12 -0400

    configure login and logout to use cookie in consumer

--- ParkConsumer/Controllers/HomeController.cs
+++ ParkConsumer/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -64,6 +67,14 @@ namespace ParkConsumer.Controllers
             {
                 return View();
             }
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, objUser.UserName));
+            identity.AddClaim(new Claim(ClaimTypes.Role, objUser.Role));
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+
             HttpContext.Session.SetString("JWToken", objUser.Token);
             return RedirectToAction("Index");
 
@@ -88,12 +99,20 @@ namespace ParkConsumer.Controllers
 
         }
 
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
+            await HttpContext.SignOutAsync();
             HttpContext.Session.SetString("JWToken", "");
             return RedirectToAction("Index");
 
         }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
 
 
     }
